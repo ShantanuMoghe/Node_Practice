dfd433ede865f78fb6f1933bc3bbf98573267e93
Shantanu Moghe <mogheshantanu@gmail.com>
2022-08-07 21:08:38 +0530

    changes

--- fs_applications.js
+++ fs_applications.js
@@ -4,10 +4,12 @@
  * !!!!AVOID CALLBACK HELL!!!!
 */
 const {readFile, writeFile} = require("fs").promises
+const { writeFileSync } = require("fs");
 
 const exec = async ()=>{
     try{
         const first = await readFile('./file-content/first_readable.txt', 'utf8');
+        
         const second =  await writeFile('./file-content/second_file.txt', 'THIS IS DATA!!', 'utf8');
         console.log(first, 'Read this file');
     } catch(error){
@@ -15,4 +17,12 @@ const exec = async ()=>{
     }
 }
 
+const createBigFile = ()=>{
+    for(let i=0; i<=10000; i++){
+        writeFileSync('./file-content/third_file.txt', `hello data ${i}\n`, {flag:'a'});
+    }
+}
+
 exec() ;
+
+createBigFile();
